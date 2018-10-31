9d55d53b06af9f62ec2000cea4ec5cf11c3b6172
sredorta <sergi.redorta@hotmail.com>
2018-10-31 18:48:27 +0100

    Working on admin

--- src/app/_members/admin-members/admin-members.component.ts
+++ src/app/_members/admin-members/admin-members.component.ts
@@ -5,6 +5,7 @@ import {User} from "../../_models/user";
 import {Role} from "../../_models/role";
 
 import { Subscription } from 'rxjs';
+import { Account } from '../../_models/account';
 
 @Component({
   selector: 'app-admin-members',
@@ -14,6 +15,7 @@ import { Subscription } from 'rxjs';
 export class AdminMembersComponent implements OnInit {
   users : User[] = new Array<User>();
   roles : Role[] = new Array<Role>();
+  accounts : string[] = ["Pré-inscrit", "Membre", "Admin"];
   userSelected : User = new User(null);
   usersCount :number;
   private _subscriptions : Subscription[] = new Array<Subscription>();
@@ -41,6 +43,39 @@ export class AdminMembersComponent implements OnInit {
     console.log("Update selected");
     console.log($event);
   }
+
+  //Handle Role actions
+  roleActionHandler(event) {
+    if (event.action == "add") {
+      //TODO:: Do the adminService first
+      let newRole = this.roles.find(i => i.id == event.role);
+      let user = this.users.find(i => i.id == event.id);
+      user.roles.push(newRole);
+    }
+    if (event.action == "remove") {
+      //TODO:: DO admin service
+      let newRole = this.roles.find(i => i.id == event.role);
+      let user = this.users.find(i => i.id == event.id);
+      let index = user.roles.indexOf(newRole);
+      user.roles.splice(index,1);
+    }
+  }
+
+  //Handle Account actions
+  accountActionHandler(event) {
+    if (event.action == "add") {
+      //TODO:: Do the adminService first and get the new account data and push account
+      let myAccount = new Account({id:1000,profile_id:event.id,access:event.account})
+
+      let user = this.users.find(i => i.id == event.id);
+      user.accounts.push(myAccount);    
+    }
+    if (event.action == "remove") {
+      let user = this.users.find(i => i.id == event.id);
+      let index = user.accounts.indexOf(user.accounts.find(i=> i.access == event.account));
+      user.accounts.splice(index,1);
+    }
+  } 
 
   ngOnDestroy() {    
     //Unsubscribe to all
--- src/app/_members/member-item-detail-admin/member-item-detail-admin.component.ts
+++ src/app/_members/member-item-detail-admin/member-item-detail-admin.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {ThemePalette} from '@angular/material/core';
 import {User} from '../../_models/user';
 import {Role} from '../../_models/role';
 
@@ -10,19 +11,45 @@ import {Role} from '../../_models/role';
 export class MemberItemDetailAdminComponent implements OnInit {
   //Inputs
   @Input() member: User;
-  @Input() roles : Role;
+  @Input() roles : Role[] = new Array<Role>();
+  @Input() accounts: Array<string> = new Array<string>();
+  @Output() actionRole = new EventEmitter<any>();
+  @Output() actionAccount = new EventEmitter<any>();
 
-  constructor() { }
+  constructor() { 
+
+  }
 
   ngOnInit() {
   }
+
   //Return if member has a certain role id
   hasRole(id:number) {
-    console.log("Finding role id: " + id);
     let result = this.member.roles.find(i => i.id == id);
     if (result == undefined) return false;
-    console.log("Returning true");
     return true;
+  }
+  hasAccount(access:string) {
+    let result = this.member.accounts.find(i => i.access == access);
+    if (result == undefined) return false;
+    return true;
+  }
+
+  //Emit outputs
+  private _removeRole(role:number) {
+    this.actionRole.emit({action:"remove", role:role, id:this.member.id});
+  }
+
+  private _addRole(role:number) {
+    this.actionRole.emit({action:"add", role:role, id:this.member.id});
+  }
+
+  private _removeAccount(account:string) {
+    this.actionAccount.emit({action:"remove", account:account, id:this.member.id});
+  }
+
+  private _addAccount(account:string) {
+    this.actionAccount.emit({action:"add", account:account, id:this.member.id});
   }
 
 }
